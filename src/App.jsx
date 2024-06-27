f701b51ca16370882a2c793e8b8ed9ad326a9da7
kabindrr <chaina_kabindra@yahoo.com>
2024-06-27 10:30:20 +1000

    setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <h1>
+        Hello from developers entry point
+        <h2>Personal Portfolio coming</h2>
+      </h1>
+    </>
+  );
+};
+
+export default App;
